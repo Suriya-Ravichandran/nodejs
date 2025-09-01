d3fda3c0c4b3bf55c067ca875f6e170199cd555a
suriyaravichandran <rsuriya119@gmail.com>
2025-09-01 20:24:25 +0530

    update

--- batch2/http/server.js
+++ batch2/http/server.js
@@ -1,11 +1,27 @@
+// import http module
 const http = require('http')
 
+// create a nodejs server
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/plain'})
-    res.end('hello world')
+    // create urls
+    if (req.url==="/"){
+        res.writeHead(200,{'content-type':'text/plain'})
+        res.end('Welcome to home page')
+    }
+    else if (req.url==="/about"){
+        res.writeHead(200,{'content-type':'text/plain'})
+        res.end('This is a about us page')
+    }
+    
 })
 
+// your computer port
 const port=3000
-server.listen(port,'localhost',()=>{
-    console.log(`server running at http://localhost:${port}`)
+// your computer ip
+const host="192.168.1.11"
+
+
+// nodejs server listen on localhost:3000
+server.listen(port,host,()=>{
+    console.log(`server running at http://${host}:${port}`)
 })
